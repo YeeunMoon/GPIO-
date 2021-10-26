56c51a68d2e4733c9829749e01e8775f76399758
YeeunMoon <myeeun98@naver.com>
2021-10-26 06:28:04 +0100

    10-26-21

--- gpioled.py
+++ gpioled.py
@@ -2,10 +2,30 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 
-LED = 11
+LED_RED = 3
+LED_GREEN = 5
+LED_BLUE = 7
 
-GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
-GPIO.output(LED, GPIO.HIGH)
-time.sleep(10)
-GPIO.output(LED, GPIO.LOW)
-time.sleep(5)
+GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+
+while 1:
+
+	#RED
+	GPIO.output(5, GPIO.HIGH)
+	time.sleep(3)
+	GPIO.output(5, GPIO.LOW)
+	time.sleep(1)
+
+	#BLUE
+	GPIO.output(7, GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(7, GPIO.LOW)
+	time.sleep(1)
+
+	#RED
+	GPIO.output(3, GPIO.HIGH)
+	time.sleep(2)
+	GPIO.output(3, GPIO.LOW)
+	time.sleep(1)
